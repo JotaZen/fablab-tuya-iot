eef101e3c9b8393162c131bc54ba2b2238df5077
JotaZen <jordyzen@gmail.com>
2025-09-14 17:53:23 -0300

    remove ocnfig .py

--- scripts/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-# Central config defaults so modules can work even if env vars are not exported
-HA_URL = os.environ.get("HA_URL", "http://localhost:8123")
-HA_TOKEN = os.environ.get("HA_TOKEN", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiI3NmNmNGVkMjM1ODc0YzY5YWJkMzM3MmM3NzYyYmUxYyIsImlhdCI6MTc1Nzg3NjMzOSwiZXhwIjoyMDczMjM2MzM5fQ.kO4V01lih7PxmuC-CdJviZmavXRiAj_lXdHeC8jH5WQ")
